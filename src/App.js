0753cb24554ffec80492ce0d249c75471707f693
Sergio <sergio.vargas@aplaceformom.com>
2020-02-20 07:41:38 -0600

    Code refactoring

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import NewsContainer from './containers/NewsContainer';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>
+					News Feed
+				</h1>
+			</header>
+			<NewsContainer />
+		</div>
+	);
 }
 
 export default App;
