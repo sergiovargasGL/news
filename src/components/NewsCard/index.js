6ca51e5b9a5cdce550d87779fbde19acbe436a22
Sergio <sergio.vargas@aplaceformom.com>
2020-02-20 07:39:26 -0600

    NewsCard styles improvements and proptypes added

--- src/components/NewsCard/index.js
+++ src/components/NewsCard/index.js
@@ -1,29 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
 import { format } from 'date-fns';
 import './NewsCard.css';
 
-const NewsCard = ({article}) => {
-	const buildDate = (date) =>{
+const NewsCard = ({ article }) => {
+	const buildDate = (date) => {
 		return format(new Date(date), 'MMMM d, yyyy');
 	};
 
-	return (<Card>
-		<CardBody>
-			<CardTitle>{article.title}</CardTitle>
-		</CardBody>
-		<CardImg top width="50%" src={article.urlToImage} alt="Card image cap" />
-		<CardBody>
-			<div className="author-source-row">
-				<CardText>{article.source.name}</CardText>
-				<CardText>By {article.author}</CardText>
-			</div>
-			<CardText>{article.description}</CardText>
-			<CardText>
-				<small className="text-muted">{buildDate(article.publishedAt)}</small>
-			</CardText>
-		</CardBody>
-	</Card>);
+	const sliceAuthor = (author) => author.length > 15 ? `${author.slice(0, 13)}...` : author;
+
+	return (
+		<Card>
+			<CardBody>
+				<a href={article.url}>
+					<CardTitle className="article-title">{article.title}</CardTitle>
+				</a>
+			</CardBody>
+			<CardImg top className="article-image" src={article.urlToImage} alt="Card image cap" />
+			<CardBody>
+				<div className="author-source-row">
+					<CardText>{article.source.name}</CardText>
+					<CardText>{article.author ? `By ${sliceAuthor(article.author)}` : 'Unknown author'}</CardText>
+				</div>
+				<CardText className="article-description">{article.description}</CardText>
+				<CardText>
+					<small className="text-muted">{buildDate(article.publishedAt)}</small>
+				</CardText>
+			</CardBody>
+		</Card>
+	);
+};
+
+NewsCard.propTypes = {
+	article: PropTypes.shape({
+		source: PropTypes.shape({
+			id: PropTypes.oneOfType([
+				PropTypes.string,
+				PropTypes.object
+			]),
+			name: PropTypes.string
+		}),
+		author: PropTypes.string,
+		title: PropTypes.string,
+		description: PropTypes.string,
+		url: PropTypes.string,
+		urlToImage: PropTypes.string,
+		publishedAt: PropTypes.string,
+		content: PropTypes.string,
+	})
 };
 
 export default NewsCard;
